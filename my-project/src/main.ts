c48f5802a04dd7c56e29519e88ee967be8ced2a0
vinividivicilala <aji862391@gmail.com>
2025-08-20 12:02:19 +0700

    Delete my-project directory

--- my-project/src/main.ts
+++
@@ -1,9 +0,0 @@
-function showHalo() {
-  const root = document.getElementById("ts-root");
-  if (root) {
-    root.innerHTML = `<h1 style="color:white; text-align:center; margin-top:20px;">Halo</h1>`;
-  }
-}
-
-// jalankan saat halaman selesai load
-document.addEventListener("DOMContentLoaded", showHalo);
