26d5e7790b699e697655e46a05a67b38709e13a3
vinividivicilala <aji862391@gmail.com>
2025-08-21 13:04:48 +0700

    Update next.config.js

--- my-project/next.config.js
+++ my-project/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Mengizinkan file static di public folder
+  async rewrites() {
+    return [
+      {
+        source: '/legacy',
+        destination: '/index.html',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
