443673999d911183909660605c49ddbb5c1e0edf
vinividivicilala <aji862391@gmail.com>
2025-08-21 12:01:11 +0700

    Update tailwind.config.js

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*index.html",
   ],
   theme: {
     extend: {
