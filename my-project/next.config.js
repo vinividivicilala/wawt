8f21da79151cc0dab7fea7aa5978508e54ab5d47
vinividivicilala <aji862391@gmail.com>
2025-08-21 19:26:22 +0700

    Update next.config.js

--- my-project/next.config.js
+++ my-project/next.config.js
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // HAPUS output: 'export' untuk mode full
   reactStrictMode: true,
   swcMinify: true,
   compress: true,
-  poweredByHeader: true, // Biarkan header "X-Powered-By: Next.js"
-  // Optimasi gambar tetap aktif
+  poweredByHeader: true,
   images: {
     formats: ['image/webp', 'image/avif'],
-    domains: [], // Tambahkan domain external jika perlu
+    domains: [],
   },
-  // Experimental features (opsional)
   experimental: {
-    optimizeCss: true, // Optimasi CSS
+    optimizeCss: true,
   },
 }
 
