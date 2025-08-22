37c51aee9c2f25789ce0d3a6d6c4cc55046e5a4d
vinividivicilala <aji862391@gmail.com>
2025-08-22 11:28:46 +0700

    Update next.config.mjs

--- my-project/next.config.mjs
+++ my-project/next.config.mjs
@@ -1,3 +1,4 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -12,4 +13,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
